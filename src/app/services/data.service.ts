9bb8d390a8fe97a02d7dab782e6028ac8ce65376
Dani2097 <39124380+Dani2097@users.noreply.github.com>
2020-05-12 22:29:34 +0200

    Update data.service.ts

--- src/app/services/data.service.ts
+++ src/app/services/data.service.ts
@@ -10,8 +10,5 @@ export class DataService {
 
   constructor( private http: HttpClient ) {}
 
-  getUsers() {
-    return this.http.get<any[]>('https://jsonplaceholder.typicode.com/users');
-  }
 
 }
